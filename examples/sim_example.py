9b4f4784dfc192228c803d5fc734897361542f91
JoostVn <joost.venselaar@hotmail.com>
2025-11-12 20:02:12 +0100

    implement movement and trails

--- examples/sim_example.py
+++ examples/sim_example.py
@@ -8,11 +8,18 @@ from math import pi
 
 
 
+# TODO: bounce agents on the border
+# TODO: agent membership in arrays or with membership arr (note: maybe membership arr is easier to change?)
+# TODO: jist get_neigbours function (used in blur and trail following)
+# TODO: why are the trails so dull? fix.
+# TODO: debug array: override all drawing to just display one array (for example, debug trails)
+
+
 @jit(float64[:,:](float64[:,:], float64), nopython=True, parallel=True)
 def blur_array(arr, fact):
     """
     Blur a 2d array by combining the value in each cell with the mean value of the
-    cells aruond it.
+    cells around it.
     """
     for i in prange(arr.shape[0]):
         for j in prange(arr.shape[1]):
@@ -32,63 +39,80 @@ class Simulation:
         self.env_dim = env_dim
         self.window_size = window_size
 
-        self.arr = np.zeros((*self.env_dim, 3))
-
         # Settings
         self.brightness = 0.1
         self.blur_factor = 0
         self.decay = 0
+        self.agent_speed = 0.2
+        self.randomness = 0.1
 
         # Agents
-        self.agent_pos = np.random.randint(
-            *env_dim, size=(num_agent_groups, num_agents_per_group, 2))
-        self.agent_angle = np.random.uniform(
-            0, 2*pi, (num_agent_groups, num_agents_per_group))
-        self.agent_trail = np.zeros(
-            (num_agent_groups, *self.env_dim))
-        self.agent_col = np.array([
-            Color.random_dull() for i in range(num_agent_groups)])
+        self.num_agent_groups = num_agent_groups
+        self.num_agents_per_group = num_agents_per_group
 
-    def blur_test(self):
-        """
-        Blur the R, G, and B values of self.arr individually
-        """
-        # TODO: move blur to update
-        for c in range(3):
-            self.arr[:,:,c] = blur_array(self.arr[:,:,c], self.blur_factor)
-        self.arr = self.arr * (1-self.decay)
+        self.agent_pos = np.full((
+            num_agent_groups, num_agents_per_group, 2), np.divide(self.env_dim, 2))
+        self.agent_angles = np.random.uniform(
+            0, 2*pi, (num_agent_groups, num_agents_per_group))
+        self.agent_trails = np.zeros(
+            (num_agent_groups, *self.env_dim))
+        
+        self.agent_col = Color.random_different(num_agent_groups)
+
+    @property
+    def group_idx(self):
+        return np.arange(self.num_agent_groups)
 
     def update(self):
         
         # Update agent angle
         # TODO
+        self.agent_angles = self.agent_angles + np.random.uniform(
+            low=-self.randomness,
+            high=self.randomness,
+            size=(self.num_agent_groups, self.num_agents_per_group))
 
         # Update agent pos
-        # TODO
+        self.agent_pos[:,:,0] += self.agent_speed * np.cos(self.agent_angles)
+        self.agent_pos[:,:,1] += self.agent_speed * np.sin(self.agent_angles)
 
-        # Update agent trails (new pos, blur, decay)
-        # TODO
+        # Bounce out of bound agents
+        agent_x = self.agent_pos[:,:,0].flatten()
+        agent_y = self.agent_pos[:,:,1].flatten()
 
-   
+        idx_x_lower = self.agent_pos[:,:,0] < 0
+        # TODO
+      
+
+        # Add new positions to agent trails
+        for i in self.group_idx:
+            agent_int_pos = tuple(self.agent_pos[i].astype(int).T)
+            self.agent_trails[i, *agent_int_pos] = 1
+
+        # Blur and apply decay to agent trails
+        for i in self.group_idx:
+            self.agent_trails[i] = blur_array(self.agent_trails[i], self.blur_factor)
+
+        self.agent_trails = self.agent_trails * (1 - self.decay)
 
     def draw(self, screen, zoom, pan_offset):
         
+        # Initialize color array
+        arr_draw_rgb = arr_draw_rgb = np.zeros((*self.env_dim, 3))
 
-
-        # apply agent color to trails and combine
-        # TODO
-
-        arr_draw_rgb = self.arr
-
-        # 
+        # apply agent color to trails and combine into color array
+        for i in self.group_idx:
+            trail_col = self.agent_col[i] * self.agent_trails[i,:,:,np.newaxis]
+            arr_draw_rgb += trail_col
         
+        arr_draw_rgb = arr_draw_rgb / self.num_agent_groups
+        
+        # Place agents on color array
+        agent_x = tuple(self.agent_pos[:,:,0].flatten().astype(int))
+        agent_y = tuple(self.agent_pos[:,:,1].flatten().astype(int))
+        arr_draw_rgb[agent_x, agent_y, :] = 255
 
-        # Place agents on arr
-        # TODO
-
-         
-
-        # Draw zoomed/panned rgb arr
+        # Adjust brightness and draw zoomed/panned color array
         arr_draw_rgb = arr_draw_rgb * self.brightness
         arr_surface = pygame.surfarray.make_surface(arr_draw_rgb)
         zoomed_window_size = tuple(np.multiply(zoom, self.window_size).astype(int))
@@ -112,39 +136,55 @@ class App(Application):
 
 
 def gui_test():
-    window_size = (150,150)
+    window_size = (400,400)
     simulation = Simulation(
-        env_dim=(50, 50),
+        env_dim=(200, 200),
         window_size=window_size,
-        num_agent_groups=2,
-        num_agents_per_group=10)
+        num_agent_groups=5,
+        num_agents_per_group=20)
     app = App(window_size, simulation)
 
     app.set_gui([
         Slider(
             obj=simulation,
             attribute='brightness',
-            domain=(0, 255),
-            default=50,
+            domain=(0, 1),
+            default=0.3,
             pos=(10, 10),
             width=60,
             height=20),
         Slider(
             obj=simulation,
             attribute='blur_factor',
-            domain=(0, 1),
-            default=0,
+            domain=(0, 0.5),
+            default=0.03,
             pos=(10, 20),
             width=60,
             height=20),
         Slider(
             obj=simulation,
             attribute='decay',
-            domain=(0, 1),
-            default=0,
+            domain=(0, 0.2),
+            default=0.002,
             pos=(10, 30),
             width=60,
-            height=20)
+            height=20),
+        Slider(
+            obj=simulation,
+            attribute='agent_speed',
+            domain=(-2, 2),
+            default=0.6,
+            pos=(10, 40),
+            width=60,
+            height=20),
+        Slider(
+            obj=simulation,
+            attribute='randomness',
+            domain=(0, 1),
+            default=0.0,
+            pos=(10, 50),
+            width=60,
+            height=20),
     ])
 
     app.run()
