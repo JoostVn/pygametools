ac3c2f333348c344b92b313f5867fb6b5f02245b
JoostVn <joost.venselaar@hotmail.com>
2025-11-12 11:40:48 +0100

    started with sim implementation

--- examples/sim_example.py
+++ examples/sim_example.py
@@ -4,6 +4,8 @@ from pygametools.gui.base import Application
 from pygametools.gui.elements import Button, Slider, Label
 import numpy as np
 from numba import jit, float64, int32, boolean, prange
+from math import pi
+
 
 
 @jit(float64[:,:](float64[:,:], float64), nopython=True, parallel=True)
@@ -21,35 +23,74 @@ def blur_array(arr, fact):
 
 class Simulation:
 
-    def __init__(self, env_dim: tuple[int, int], window_size: tuple[int, int]):
+    def __init__(
+            self,
+            env_dim: tuple[int, int],
+            window_size: tuple[int, int],
+            num_agent_groups: int,
+            num_agents_per_group: int):
         self.env_dim = env_dim
         self.window_size = window_size
-        # self.arr = np.zeros((*self.env_dim, 3))
-        self.arr = np.random.uniform(0, 1, size=(*self.env_dim, 3))
+
+        self.arr = np.zeros((*self.env_dim, 3))
+
+        # Settings
         self.brightness = 0.1
         self.blur_factor = 0
         self.decay = 0
+
+        # Agents
+        self.agent_pos = np.random.randint(
+            *env_dim, size=(num_agent_groups, num_agents_per_group, 2))
+        self.agent_angle = np.random.uniform(
+            0, 2*pi, (num_agent_groups, num_agents_per_group))
+        self.agent_trail = np.zeros(
+            (num_agent_groups, *self.env_dim))
+        self.agent_col = np.array([
+            Color.random_dull() for i in range(num_agent_groups)])
 
     def blur_test(self):
         """
         Blur the R, G, and B values of self.arr individually
         """
+        # TODO: move blur to update
         for c in range(3):
             self.arr[:,:,c] = blur_array(self.arr[:,:,c], self.blur_factor)
         self.arr = self.arr * (1-self.decay)
 
     def update(self):
-        pass
-        # self.arr = np.random.uniform(0, 1, size=(*self.env_dim, 3))
+        
+        # Update agent angle
+        # TODO
+
+        # Update agent pos
+        # TODO
+
+        # Update agent trails (new pos, blur, decay)
+        # TODO
+
+   
 
     def draw(self, screen, zoom, pan_offset):
+        
 
 
-        self.blur_test()
+        # apply agent color to trails and combine
+        # TODO
 
+        arr_draw_rgb = self.arr
 
-        arr_rgb = self.arr * self.brightness
-        arr_surface = pygame.surfarray.make_surface(arr_rgb)
+        # 
+        
+
+        # Place agents on arr
+        # TODO
+
+         
+
+        # Draw zoomed/panned rgb arr
+        arr_draw_rgb = arr_draw_rgb * self.brightness
+        arr_surface = pygame.surfarray.make_surface(arr_draw_rgb)
         zoomed_window_size = tuple(np.multiply(zoom, self.window_size).astype(int))
         arr_surface = pygame.transform.scale(arr_surface, zoomed_window_size)
         rect = arr_surface.get_rect()
@@ -72,7 +113,11 @@ class App(Application):
 
 def gui_test():
     window_size = (150,150)
-    simulation = Simulation(env_dim=(50, 50), window_size=window_size)
+    simulation = Simulation(
+        env_dim=(50, 50),
+        window_size=window_size,
+        num_agent_groups=2,
+        num_agents_per_group=10)
     app = App(window_size, simulation)
 
     app.set_gui([
