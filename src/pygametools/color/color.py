16dfa9a66189dc087fa1c5258813ff0f71307087
Joost Venselaar <joost.venselaar@hotmail.com>
2026-03-04 21:11:33 +0100

    Moved colorpreview to the Color module

--- src/pygametools/color/color.py
+++ src/pygametools/color/color.py
@@ -1,5 +1,7 @@
+from inspect import getmembers
+from matplotlib import pyplot as plt
 import numpy as np
-from math import ceil
+from math import ceil, floor, sqrt
 from itertools import product
 
 
@@ -86,3 +88,76 @@ class ColorGradient:
         value = min(1, max(0, value))
         return self.c1 + self.width * value
 
+
+def colorpreview():
+    """
+    Function that neatly plots a preview of all colors in the PygamePlot
+    module in addition to previews of randomly generated colors.
+    """
+    fig, axs = plt.subplots(
+        1, 2, figsize=(8,7), gridspec_kw={'width_ratios':(1,1.8)})
+
+    # ______________________ Named colors
+    ax = axs[1]
+
+    # Fetch colors and colornames from Color class
+    names, colors = [], []
+    for i in getmembers(Color):
+        if type(i[1]) is tuple:
+            names.append(i[0])
+            colors.append(np.array(i[1])/255)
+
+    # Determine points for scatterplots
+    nr_colors = len(colors)
+    nr_columns = floor(sqrt(nr_colors))
+    nr_rows = ceil(nr_colors / nr_columns)
+    points = [
+        (col, row) for row in range(nr_rows-1, -1, -1)
+        for col in range(nr_columns)]
+
+    # Create annotated scatterplot for colors
+    for point, color, name in zip(points[:nr_colors], colors, names):
+        ax.scatter(*point, s=1200, c=[color], marker='s')
+        text_point = np.add(point, (0, 0.4))
+        ax.text(*text_point, name, horizontalalignment='center', fontsize=10)
+    ax.set_xlim(-0.5, nr_columns-0.5)
+    ax.set_ylim(-0.5, nr_rows-0.4)
+    ax.axis('off')
+    ax.tick_params(left=False,bottom=False,labelleft=False,labelbottom=False)
+    ax.set_title('Named colors', fontsize=19, pad=20)
+
+    # ______________________ Random colors
+    ax = axs[0]
+    n = 4
+
+    # Create point coordinates for color scatter plots
+    vibr_points = np.indices((n, n)).reshape(2, n**2)
+    vibr_colors = np.array([Color.random_vibrant() for i in range(n**2)]) / 255
+    dull_points = np.vstack((vibr_points[0], vibr_points[1] + n + 2))
+    dull_colors = np.array([Color.random_dull() for i in range(n**2)]) / 255
+    diff_points = np.vstack((dull_points[0], dull_points[1] + n + 2))
+    diff_colors = Color.random_different(n**2) / 255
+
+    # Scatterplot showing different colors
+    ax.scatter(*vibr_points, c=vibr_colors, s=350, marker='s')
+    ax.scatter(*dull_points, c=dull_colors, s=350, marker='s')
+    ax.scatter(*diff_points, c=diff_colors, s=350, marker='s')
+    ax.text((n-1)/2, n, 'Random vibrant',
+            horizontalalignment='center', fontsize=11)
+    ax.text((n-1)/2, (n + 2) + n, 'Random dull',
+            horizontalalignment='center', fontsize=11)
+    ax.text((n-1)/2, 2 * (n + 2) + n, 'Random different',
+            horizontalalignment='center', fontsize=11)
+    ax.set_xlim(-1.5, n + 0.5)
+    ax.set_ylim(-1, 3 * (n + 2)-1)
+    ax.tick_params(left=False,bottom=False,labelleft=False,labelbottom=False)
+    ax.axis('off')
+    ax.set_title('Random colors', fontsize=19, pad=20)
+
+
+    fig.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    colorpreview()
