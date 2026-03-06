b2825a109febf05f173d262623bedd64ad69ffdc
Joost Venselaar <joost.venselaar@hotmail.com>
2026-03-06 23:23:49 +0100

    More domain control in the test script

--- examples/plots_dev.py
+++ examples/plots_dev.py
@@ -23,7 +23,9 @@ class PlotTestApp(Application):
         self._plot_ypos = 0
         self._plot_xdim = 0
         self._plot_ydim = 0
+        self._xdom_min = 0
         self._xdom_max = 0
+        self._ydom_min = 0
         self._ydom_max = 0
         self._num_yticks = 0
         self._num_xticks = 0
@@ -83,26 +85,6 @@ class PlotTestApp(Application):
         self.plot.metrics.dim = (old_dim[0], val)
         
     @property
-    def xdom_max(self):
-        return self._xdom_max
-    
-    @xdom_max.setter
-    def xdom_max(self, val: tuple[int, int]):
-        self._xdom_max = val
-        prev_xdom = self.plot.metrics.xdom
-        self.plot.metrics.xdom = (prev_xdom[0], val)
-
-    @property
-    def ydom_max(self):
-        return self._ydom_max
-    
-    @ydom_max.setter
-    def ydom_max(self, val: tuple[int, int]):
-        self._ydom_max = val
-        prev_ydom = self.plot.metrics.ydom
-        self.plot.metrics.ydom = (prev_ydom[0], val)
-
-    @property
     def num_xticks(self):
         return self._num_xticks
     
@@ -120,6 +102,50 @@ class PlotTestApp(Application):
     def num_yticks(self, val):
         self._num_yticks = int(val)
         self.plot.axisy.set_num_ticks(self._num_yticks)
+
+    @property
+    def xdom_min(self):
+        return self._xdom_min
+
+    @xdom_min.setter
+    def xdom_min(self, val: float):
+        val = -(10 ** val)
+        self._xdom_min = val
+        prev_xdom = self.plot.metrics.xdom
+        self.plot.metrics.xdom = (val, prev_xdom[1])
+
+    @property
+    def xdom_max(self):
+        return self._xdom_max
+    
+    @xdom_max.setter
+    def xdom_max(self, val: float):
+        val = 10 ** val
+        self._xdom_max = val
+        prev_xdom = self.plot.metrics.xdom
+        self.plot.metrics.xdom = (prev_xdom[0], val)
+
+    @property
+    def ydom_min(self):
+        return self._ydom_min
+
+    @ydom_min.setter
+    def ydom_min(self, val: float):
+        val = -(10 ** val)
+        self._ydom_min = val
+        prev_ydom = self.plot.metrics.ydom
+        self.plot.metrics.ydom = (val, prev_ydom[1])
+
+    @property
+    def ydom_max(self):
+        return self._ydom_max
+    
+    @ydom_max.setter
+    def ydom_max(self, val: float):
+        val = 10 ** val
+        self._ydom_max = val
+        prev_ydom = self.plot.metrics.ydom
+        self.plot.metrics.ydom = (prev_ydom[0], val)
     
 
 def main():
@@ -156,16 +182,23 @@ def main():
         app, 'plot_ydim', domain=(80, 390), default=200, pos=(10, 55),
         width=60, height=20),
     Slider(
-        app, 'xdom_max', domain=(5, 20), default=5, pos=(10, 70),
+        app, 'num_xticks', domain=(1, 20), default=6, pos=(10, 85),
         width=60, height=20),
     Slider(
-        app, 'ydom_max', domain=(5, 20), default=5, pos=(10, 85),
+        app, 'num_yticks', domain=(1, 20), default=4, pos=(10, 100),
+        width=60, height=20),
+    
+    Slider(
+        app, 'xdom_min', domain=(-5, 6), default=-1, pos=(10, 130),
         width=60, height=20),
     Slider(
-        app, 'num_xticks', domain=(1, 20), default=6, pos=(10, 100),
+        app, 'xdom_max', domain=(-5, 6), default=1, pos=(10, 145),
         width=60, height=20),
     Slider(
-        app, 'num_yticks', domain=(1, 20), default=4, pos=(10, 115),
+        app, 'ydom_min', domain=(-5, 6), default=-1, pos=(10, 175),
+        width=60, height=20),
+    Slider(
+        app, 'ydom_max', domain=(-5, 6), default=1, pos=(10, 190),
         width=60, height=20),
     ])
     
